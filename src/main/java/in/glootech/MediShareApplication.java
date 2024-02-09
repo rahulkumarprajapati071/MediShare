c72ae63ae6a8c460343e873bb0fcc3512dc3bef3
Rahul <rktechnicalgroup69@gmail.com>
2024-02-09 23:46:53 +0530

    0.0.1 SNAPSHOT Initial Commit

---
+++ src/main/java/in/glootech/MediShareApplication.java
@@ -0,0 +1,13 @@
+package in.glootech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediShareApplication.class, args);
+	}
+
+}
